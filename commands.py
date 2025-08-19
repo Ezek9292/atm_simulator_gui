eaad95efbea02a216f9338f228a8fd21b3203170
ezekiel <ezekdonkor@gmail.com>
2025-08-19 12:12:03 +0000

    Last commit

--- commands.py
+++ commands.py
@@ -1,3 +1,15 @@
 from tkinter import messagebox
-def check_balance():
-    messagebox.showinfo(title="Check Balance", message=f"Your balance is {34526}")
+import csv
+def check_balance(account_number):
+    # open csv file
+    csv_file = open(file="accounts.csv", mode="r")
+    # read csv file
+    accounts = csv.DictReader(csv_file)
+    # print(list(accounts))
+    for account in accounts:
+        if account_number == account["account_number"]:
+            # show balance
+            messagebox.showinfo(title="Check Balance", message=f"Your balance is {account["balance"]}")
+            return
+    # Show user an account not found
+    messagebox.showerror(title="Check Balance", message=f"Account number {account_number} does not exist")
--- main.py
+++ main.py
@@ -9,13 +9,17 @@ root.title("ATM Simulator")
 # specify main window size
 root.geometry("500x400")
 
+# Add account number entry
+account_number_entry = tk.Entry(root, width=50)
+account_number_entry.pack(side="top")
+
 # Add check balance button
-check_balance_btn =tk.Button(root,text="Chack Balance", command=check_balance)
+check_balance_btn =tk.Button(root, text="Chack Balance", command=lambda : check_balance(account_number=account_number_entry.get()))
 check_balance_btn.pack(side="top")
 
 # Add deposit button
 deposit_btn =tk.Button(root, text="Deposit")
-deposit_btn.pack(side="top")
+deposit_btn.pack(side="bottom")
 
 # Open the main window
 root.mainloop()
